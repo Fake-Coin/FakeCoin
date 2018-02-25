6c2b8e1cae5e118945d4256787b42c53a6a9d47f
Neil Vallon <neilvallon@gmail.com>
2018-02-25 11:01:41 -0600

    fakecoin: only allow -qt and fakd peers until a better solution is found

--- src/main.cpp
+++ src/main.cpp
@@ -3685,17 +3685,22 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             pfrom->cleanSubVer = SanitizeString(pfrom->strSubVer);
         }
 
-        // Disconnect certain incompatible clients
-        const char *badSubVers[] = { "/potcoinseeder", "/reddcoinseeder", "/worldcoinseeder" };
-        for (int x = 0; x < 3; x++)
+        // Whitelist known clients.
+        const char *knownSubVers[] = { "/Mhoy Money", "/btcwire" };
+        bool allowedClient = false;
+        for (int x = 0; x < 2; x++)
         {
-            if (pfrom->cleanSubVer.find(badSubVers[x], 0) == 0)
+            if (pfrom->cleanSubVer.find(knownSubVers[x], 0) == 0)
             {
-                LogPrintf("invalid subver %s at %s, disconnecting\n", pfrom->cleanSubVer, pfrom->addr.ToString());
-                pfrom->PushMessage("reject", strCommand, REJECT_INVALID, string("invalid client subver"));
-                pfrom->fDisconnect = true;
-                return true;
+                allowedClient = true;
+                break;
             }
+        }
+        if (!allowedClient) {
+            LogPrintf("invalid subver %s at %s, disconnecting\n", pfrom->cleanSubVer, pfrom->addr.ToString());
+            pfrom->PushMessage("reject", strCommand, REJECT_INVALID, string("invalid client subver"));
+            pfrom->fDisconnect = true;
+            return true;
         }
 
         if (!vRecv.empty())
