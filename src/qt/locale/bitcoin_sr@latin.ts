f86c52b16512abf691b9ce3c02e228b8b758ba14
Neil Vallon <neilvallon@gmail.com>
2018-03-05 04:23:47 -0600

    branding

--- src/qt/locale/bitcoin_sr@latin.ts
+++ src/qt/locale/bitcoin_sr@latin.ts
@@ -158,16 +158,16 @@
         <translation>Otvori &amp;URI...</translation>
     </message>
     <message>
-        <source>Send coins to a Litecoin address</source>
-        <translation>Pošalji novčiće na Litecoin adresu</translation>
+        <source>Send coins to a FakeCoin address</source>
+        <translation>Pošalji novčiće na FakeCoin adresu</translation>
     </message>
     <message>
         <source>&amp;Verify message...</source>
         <translation>&amp;Proveri poruku...</translation>
     </message>
     <message>
-        <source>Litecoin</source>
-        <translation>Litecoin</translation>
+        <source>FakeCoin</source>
+        <translation>FakeCoin</translation>
     </message>
     <message>
         <source>Wallet</source>
@@ -412,8 +412,8 @@
 <context>
     <name>bitcoin-core</name>
     <message>
-        <source>Litecoin Core</source>
-        <translation>Litecoin Core</translation>
+        <source>FakeCoin Core</source>
+        <translation>FakeCoin Core</translation>
     </message>
     <message>
         <source>Information</source>
