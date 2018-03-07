7ba3a4498b27bd364d8493995c8e4c2de8dce9f7
Neil Vallon <neilvallon@gmail.com>
2018-03-06 18:20:45 -0600

    build scripts

---
+++ install-linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo apt-get install build-essential libtool autotools-dev autoconf pkg-config libevent-dev
+
+# OpenSSL
+sudo apt-get install openssl libssl-dev
+
+# Boost
+sudo apt-get install libboost-all-dev
+
+# Qt
+sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools
+
+# Protocol Buffers
+sudo apt-get install libprotobuf-dev protobuf-compiler
+
+# QR Codes
+sudo apt-get install libqrencode-dev
+
+# UPnP
+sudo apt-get install libminiupnpc-dev
+
+# Berkeley DB
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install libdb4.8-dev libdb4.8++-dev
+
+./autogen.sh 
+./configure --prefix=$PWD/dist --with-gui=qt5 --with-qrencode --enable-upnp-default --enable-shared=no --disable-tests
+make -j8
+make install
--- install-mac.sh
+++ install-mac.sh
@@ -4,6 +4,8 @@ set -x
 
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig/lib/pkgconfig:/usr/local/Cellar/qt/5.10.1/lib/pkgconfig:$PKG_CONFIG_PATH
 
+# export LDFLAGS="-g -fsanitize=address -L/usr/local/opt/openssl/lib -L/usr/local/opt/boost@1.60/lib -L/usr/local/opt/qt/lib"
+
 export LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/opt/boost@1.60/lib -L/usr/local/opt/qt/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl/include -I/usr/local/opt/boost@1.60/include -I/usr/local/opt/qt/include"
 
@@ -12,7 +14,9 @@ export QR_CFLAGS="-I/usr/local/opt/qrencode/include"
 
 export CXXFLAGS="-std=c++11 -arch x86_64 -Wno-deprecated-declarations"
 export OBJCXXFLAGS="-std=c++11 -arch x86_64 -Wno-deprecated-declarations"
+# export CXXFLAGS="-std=c++11 -arch x86_64 -Wno-deprecated-declarations -O1 -g -fsanitize=address -fno-omit-frame-pointer"
+# export OBJCXXFLAGS="-std=c++11 -arch x86_64 -Wno-deprecated-declarations -O1 -g -fsanitize=address -fno-omit-frame-pointer"
 
 ./autogen.sh
-./configure --with-gui=qt5 --with-qrencode --enable-upnp-default --enable-shared=no --disable-tests --enable-debug
-make -j8 appbundle
+./configure --with-gui=qt5 --with-qrencode --enable-upnp-default --enable-shared=no --disable-tests
+make -j8 deploy
